5022dc405c40314e8865e3dd76736917f6920c68
unknown <lukaszschweda@gmail.com>
2019-11-16 14:44:38 +0100

    test calculator

---
+++ kodilla-testing/src/main/java/com/codilla/testing/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.codilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+
+    public Calculator(){
+        this.a = a;
+        this.b = b;
+    }
+    public int add (int a, int b){
+        return a + b;
+    }
+    public int subtract (int a, int b) {
+        return a - b;
+    }
+}
