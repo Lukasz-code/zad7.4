e2923ac401ed15cac3f67a39cebb9ad8e5e02302
Lukasz-code <lukaszschweda@gmail.com>
2019-11-19 16:02:14 +0100

    test

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.testing.collection;
 import com.codilla.testing.collection.OddNumbersExterminator;
 import org.junit.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CollectionTestSuite {
     @Before
@@ -18,13 +19,32 @@ public class CollectionTestSuite {
         //Given
         OddNumbersExterminator exterminator = new OddNumbersExterminator();
         ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.add(4);
-        numbers.add(2);
-        numbers.add(8);
+        numbers.add(1);
+        numbers.add(3);
         //When
-        ArrayList<Integer> evenNumbers = null;
-        ArrayList<Integer> oddNumbers = exterminator.exterminate(evenNumbers);
+        ArrayList<Integer> oddNumbers = exterminator.exterminate(numbers);
+        ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
         //Then
-        System.out.println(oddNumbers);
+        Assert.assertEquals(oddNumbers, evenNumbers);
     }
-}
+
+    @Test
+    public void testOddNumbersExterminatorNormalList() {
+        //Given
+        OddNumbersExterminator exterminator = new OddNumbersExterminator();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        //When
+        ArrayList<Integer> oddNumbers = exterminator.exterminate(numbers);
+        ArrayList<Integer> evenNumbers = new ArrayList<Integer>();
+        evenNumbers.add(2);
+        evenNumbers.add(4);
+        //Then
+        Assert.assertEquals(oddNumbers, evenNumbers);
+        }
+    }
+
