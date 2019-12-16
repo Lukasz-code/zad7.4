38dda999e90d7f0a4f4f72249fbbfebf06251d91
unknown <lukaszschweda@gmail.com>
2019-12-16 22:06:58 +0100

    7.5

--- ukodilla-steam/src/main/java/com/kodilla/steam/array/ArrayOperations.java
+++ ukodilla-steam/src/main/java/com/kodilla/steam/array/ArrayOperations.java
@@ -1,19 +1,18 @@
 package com.kodilla.steam.array;
 
-import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
 
-    public static OptionalDouble getAverage(int[] numbers) {
+    public static double getAverage(int[] numbers) {
 
          IntStream.range(0, numbers.length)
                 .map(n -> numbers[n])
                 .forEach(x -> System.out.println("numbers in Array: " + x));
 
-        OptionalDouble averageNumber = IntStream.range(0, numbers.length)
+        double averageNumber = IntStream.range(0, numbers.length)
                 .map(n -> numbers[n])
-                .average();
+                .average().getAsDouble();
         System.out.println("Average number from array: " + averageNumber);
         return averageNumber;
     }
--- ukodilla-steam/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ ukodilla-steam/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -4,8 +4,6 @@ import com.kodilla.steam.array.ArrayOperations;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.OptionalDouble;
-
 public class ArrayOperationsTestSuite {
 
     @Test
@@ -15,9 +13,9 @@ public class ArrayOperationsTestSuite {
         int[] numbers = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2};
 
         //When
-        OptionalDouble average =  ArrayOperations.getAverage(numbers);
+        double average =  ArrayOperations.getAverage(numbers);
 
         //Then
-        Assert.assertEquals(1.05, average.getAsDouble() ,0.001 );
+        Assert.assertEquals(1.05, average ,0.001 );
     }
 }
